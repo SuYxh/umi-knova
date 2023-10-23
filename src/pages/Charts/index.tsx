a213a19a0fba32413e0591e4cf6d360aefaf5079
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-10-23 16:38:32 +0800

    feat: 增加图表

--- src/pages/Charts/index.tsx
+++ src/pages/Charts/index.tsx
@@ -1,15 +1,24 @@
-import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
-import { useModel } from '@umijs/max';
 import styles from './index.less';
+import SingleLine from './components/SingleLine';
+import MultiLine from './components/MultiLine';
 
 const HomePage: React.FC = () => {
-  const { name } = useModel('global');
+  
   return (
     <PageContainer ghost>
       <div className={styles.container}>
-        <Guide name={trim(name)} />
+        <SingleLine /> 
+
+        <br />
+        <br />
+        <br />
+
+        <MultiLine />
+
+        <br />
+        <br />
+        <br />
       </div>
     </PageContainer>
   );
