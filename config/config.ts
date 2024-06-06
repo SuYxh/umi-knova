9fa681e33a8f1394881f0130839f7611583753d0
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-06-06 11:43:30 +0800

    feat: update

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@umijs/max';
 import routes from './routes';
 
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/umi-knova/' : '/',
   mock: {},
   antd: {},
   access: {},
